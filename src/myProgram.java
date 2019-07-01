77e5de9a92952f475a68055e04ab0d0e76c75403
mariamjaludi <mariam.jaludi@gmail.com>
2019-07-01 12:10:34 +0100

    first commit

---
+++ src/myProgram.java
@@ -0,0 +1,31 @@
+import java.util.Scanner; 
+
+public class myProgram {
+
+	public static void main(String[] args) { //methods
+		System.out.println("What is your name?");
+		
+		//syntax for new object
+		//type identifier = new type();
+		Scanner scanner = new Scanner(System.in);
+		String name = scanner.nextLine();
+		
+		System.out.println("Hello " + name);
+	}
+	
+
+
+}
+
+
+//class contains everything and has members
+//methods - do something
+//properties - store something (variables)
+
+//access modifier (public/private) - who can use it
+//static - No instance of a class needed (class method)
+//object - instance of a class
+
+//parameter and arguments: 
+//argument: what you pass into a method
+//parameter: variable that will store the data
